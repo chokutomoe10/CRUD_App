4cb56edb26115007ee77cdb490ee8668881362e9
chokutomoe10 <mikhaelbani7@gmail.com>
2023-11-17 16:24:16 +0700

    crud app

--- CRUD/config/database.js
+++ CRUD/config/database.js
@@ -1,28 +1,28 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "6533f3gDH2De2bfaA5bGc4BfDhHHB3Af",
+    "password": "fG3hEF6HGBfbBa-agfeC2AGh12Ff6c-a",
     "database": "railway",
-    "host": "monorail.proxy.rlwy.net",
-    "port": 31759,
+    "host": "viaduct.proxy.rlwy.net",
+    "port": 57566,
     "dialect": "mysql",
     "dialectModule": require('mysql2')
   },
   "test": {
     "username": "root",
-    "password": "6533f3gDH2De2bfaA5bGc4BfDhHHB3Af",
+    "password": "fG3hEF6HGBfbBa-agfeC2AGh12Ff6c-a",
     "database": "railway",
-    "host": "monorail.proxy.rlwy.net",
-    "port": 31759,
+    "host": "viaduct.proxy.rlwy.net",
+    "port": 57566,
     "dialect": "mysql",
     "dialectModule": require('mysql2')
   },
   "production": {
     "username": "root",
-    "password": "6533f3gDH2De2bfaA5bGc4BfDhHHB3Af",
+    "password": "fG3hEF6HGBfbBa-agfeC2AGh12Ff6c-a",
     "database": "railway",
-    "host": "monorail.proxy.rlwy.net",
-    "port": 31759,
+    "host": "viaduct.proxy.rlwy.net",
+    "port": 57566,
     "dialect": "mysql",
     "dialectModule": require('mysql2')
   }
