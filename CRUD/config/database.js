e170a92c7d6f3b9f3aea17537ad617860c74b57a
chokutomoe10 <mikhaelbani7@gmail.com>
2023-11-17 16:04:09 +0700

    new commit

--- CRUD/config/database.js
+++ CRUD/config/database.js
@@ -10,19 +10,19 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": "ehfCD2eBDH21efFEd5DeeHaf3114e1BH",
+    "password": "6533f3gDH2De2bfaA5bGc4BfDhHHB3Af",
     "database": "railway",
     "host": "monorail.proxy.rlwy.net",
-    "port": "31759",
+    "port": 31759,
     "dialect": "mysql",
     "dialectModule": require('mysql2')
   },
   "production": {
     "username": "root",
-    "password": "ehfCD2eBDH21efFEd5DeeHaf3114e1BH",
+    "password": "6533f3gDH2De2bfaA5bGc4BfDhHHB3Af",
     "database": "railway",
     "host": "monorail.proxy.rlwy.net",
-    "port": "31759",
+    "port": 31759,
     "dialect": "mysql",
     "dialectModule": require('mysql2')
   }
